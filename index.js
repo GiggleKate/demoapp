2af8d4218d267173385349c65e278e307eab6acf
GiggleKate <pattamaporn@hospital-os.com>
2017-10-02 16:32:31 +0700

    demoapp

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express()
+
+// GET method route
+app.get('/', (req, resp) => {
+    resp.send('Hello')
+
+})
+
+app.all('/health', (req, resp) => {
+resp.send('ok')
+})
+
+app.listen(8090)
